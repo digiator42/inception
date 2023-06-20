36da17b07108726aff25f7b842a1bebae5e1e05f
digiator42 <digiator2020@gmail.com>
2023-06-20 11:09:47 -0400

    my secret domain name

--- srcs/requirements/nginx/tools/certs.sh
+++ srcs/requirements/nginx/tools/certs.sh
@@ -5,5 +5,8 @@ openssl req -x509 -newkey rsa:4096 -keyout $SSL_CERTIFICATE_KEY \
 	-out $SSL_CERTIFICATE -sha256 -days 3650 -nodes \
 	-subj "/C=XX/ST=ahassan/L=AD/O=42/OU=N\A/CN=ahassan";
 
-sed -i "s|SSL_CERTIFICATE|${SSL_CERTIFICATE}|" /etc/nginx/http.d/nginx.conf && \
-sed -i "s|SSL_KEY|${SSL_CERTIFICATE_KEY}|" /etc/nginx/http.d/nginx.conf;
+sed -i -e "s|SSL_CERTIFICATE|${SSL_CERTIFICATE}|" \
+       -e "s|SSL_KEY|${SSL_CERTIFICATE_KEY}|" \
+       -e "s|DOMAIN|${DOMAIN}|" \
+       -e "s|WDOMAIN|${WDOMAIN}|" /etc/nginx/http.d/nginx.conf
+
